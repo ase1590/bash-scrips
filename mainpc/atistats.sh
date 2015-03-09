1166be01318e513a222cceaa5ff9bdf4d60da4bd
ase1590 <ase1590@hotmail.com>
2015-03-09 18:15:41 -0500

    added other files

---
+++ mainpc/atistats.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# if the script was not launched from a terminal, restart it from a terminal
+if [[ ! -t 0 ]] && [[ -x /usr/bin/x-terminal-emulator ]]; then
+   /usr/bin/x-terminal-emulator -e "bash -c \"$0 $*; read -s -p 'Press enter to continue...'\""
+   exit
+fi
+
+watch -n 2 aticonfig --pplib-cmd "get fanspeed 0" --odgc --odgt
